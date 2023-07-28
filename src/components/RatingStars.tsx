9785e3621c6cc3e7820295adb021174c2645e444
priyanshuspawar <priyansusingh306@gmail.com>
2023-07-28 15:02:17 +0530

    custom cursor added

--- src/components/RatingStars.tsx
+++ src/components/RatingStars.tsx
@@ -1,10 +1,25 @@
-import {ReactComponent as RatingStar}  from "../assets/ratingStar.svg";
+import {ReactComponent as HalfRatingStar}  from "../assets/ratingStar.svg";
+import {ReactComponent as RatingStar}  from "../assets/star.svg";
 
+interface props{
+  rating:number;
+}
 
-const RatingStars = () => {
+const RatingStars = ({rating}:props) => {
+  const isHalfRating:boolean =  Math.ceil(rating)==Math.floor(rating);
+  const mainRating = Math.floor(rating);;
+  const ratingsArray = [];
+  for (let i = 1; i <mainRating;i++){
+    ratingsArray.push(i);
+  }
   return (
-    <div>
-        <RatingStar stop-color={"black"}/>
+    <div className="flex gap-1">
+      {ratingsArray.map(()=>{
+        return(
+          <RatingStar/>
+        )
+      })}
+      {!isHalfRating&&<HalfRatingStar/>}
     </div>
   )
 }
